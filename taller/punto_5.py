10fb0d8352788e05dbd3a7d200994bb733b4592f
Lau <laurabedoyacoronado@gmail.com>
2023-08-08 17:19:12 -0500

    commit punto_5.py

---
+++ taller/punto_5.py
@@ -0,0 +1,27 @@
+import math
+
+
+class Circulo:
+    def __init__(self):
+        self.centro: tuple[int, int] = (3, -2)
+        self.radio: int = 5
+
+    def calcular_area(self):
+        return math.pi * self.radio ** 2
+
+    def calcular_perimetro(self):
+        return 2 * math.pi * self.radio
+
+    def calcular_pertenencia(self):
+        punto_x, punto_y = punto
+        centro_x, centro_y = self.centro
+        distancia = math.sqrt((punto_x - centro_x) ** 2 + (punto_y - centro_y) ** 2)
+        return distancia <= self.radio
+
+
+if __name__ == "__main__":
+    punto: tuple[int, int] = (6, -6)
+    circulo = Circulo()
+    print("AREA: ", circulo.calcular_area())
+    print("PERIMETRO: ", circulo.calcular_perimetro())
+    print("DISTANCIA: ", circulo.calcular_pertenencia())
